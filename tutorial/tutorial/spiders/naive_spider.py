84c85137fbb6ae333336a6d226ee8bc6619b8803
geekan <geekan@foxmail.com>
2014-01-11 11:02:33 -0800

    add dmoz spider

--- tutorial/tutorial/spiders/naive_spider.py
+++ tutorial/tutorial/spiders/naive_spider.py
@@ -1,6 +1,7 @@
 '''
 This is a naive spider only for example
 '''
+from scrapy.selector import Selector
 from scrapy.spider import BaseSpider as Spider
 
 
@@ -18,3 +19,24 @@ class NaiveSpider(Spider):
     def parse(self, response):
         filename = response.url.split("/")[-2]
         open(filename, 'wb').write(response.body)
+
+
+class DmozSpider(Spider):
+    name = "dmoz"
+    allowed_domains = ["dmoz.org"]
+    start_urls = [
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
+        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
+    ]
+
+    def parse(self, response):
+        sel = Selector(response)
+        sites = sel.xpath('//ul/li')
+        items = []
+        for site in sites:
+            item = DmozItem()
+            item['title'] = site.xpath('a/text()').extract()
+            item['link'] = site.xpath('a/@href').extract()
+            item['desc'] = site.xpath('text()').extract()
+            items.append(item)
+        return items
