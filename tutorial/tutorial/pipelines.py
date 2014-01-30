ff51d1037c1c044deda38407e119a0def64fa95e
geekan <geekan@foxmail.com>
2014-01-30 23:10:59 +0800

    add xml exporter

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -3,6 +3,38 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy import signals
+from scrapy.contrib.exporter import XmlItemExporter
+
+
 class TutorialPipeline(object):
     def process_item(self, item, spider):
+        return item
+
+
+class XmlExportPipeline(object):
+
+    def __init__(self):
+        self.files = {}
+
+     @classmethod
+     def from_crawler(cls, crawler):
+         pipeline = cls()
+         crawler.signals.connect(pipeline.spider_opened, signals.spider_opened)
+         crawler.signals.connect(pipeline.spider_closed, signals.spider_closed)
+         return pipeline
+
+    def spider_opened(self, spider):
+        file = open('%s_products.xml' % spider.name, 'w+b')
+        self.files[spider] = file
+        self.exporter = XmlItemExporter(file)
+        self.exporter.start_exporting()
+
+    def spider_closed(self, spider):
+        self.exporter.finish_exporting()
+        file = self.files.pop(spider)
+        file.close()
+
+    def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
