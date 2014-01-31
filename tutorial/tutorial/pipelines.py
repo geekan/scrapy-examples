279341ea8f049473da09a450c17d0183a54ff425
geekan <geekan@foxmail.com>
2014-01-31 09:24:23 +0800

    add json dump pipeline

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -40,3 +40,20 @@ class XmlExportPipeline(object):
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
+
+
+import json
+import codecs
+
+class JsonWithEncodingPipeline(object):
+
+    def __init__(self):
+        self.file = codecs.open('data_utf8.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
+        return item
+
+    def spider_closed(self, spider):
+        self.file.close()
