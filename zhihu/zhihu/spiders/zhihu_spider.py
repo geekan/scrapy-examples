f5c18f9ae8d6f4fb7d7b995f599b40a9d3da6f3e
geekan <geekan@foxmail.com>
2014-11-10 00:44:29 +0800

    zhihu: add side following data and add a XXX comment

--- zhihu/zhihu/spiders/zhihu_spider.py
+++ zhihu/zhihu/spiders/zhihu_spider.py
@@ -16,6 +16,36 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
 from zhihu.items import *
 from misc.log import *
 
+'''
+XXX：理论上，可以把所有的css rules都用dict表示（有深度）：
+
+all_css_rules = {
+    '.zm-profile-header': {
+        '.zm-profile-header-main': {
+            # 这一层就要dump出数据了，以__use标签为准
+            ‘__use’:'dump',
+            'name':'.title-section .name::text',
+            'sign':'.title-section .bio::text',
+            'location':'.location.item::text',
+            'business':'.business.item::text',
+            'employment':'.employment.item::text',
+            'position':'.position.item::text',
+            'education':'.education.item::text',
+            'education_extra':'.education-extra.item::text',
+        },
+        '.zm-profile-header-operation': {
+
+        },
+    }
+}
+'''
+
+class ZhihuCrawlerContext(object):
+
+    def __init__(self, sel=None, css_rules=None):
+        self.sel = sel
+        self.css_rules = css_rules
+
 
 class ZhihuSpider(CrawlSpider):
     name = "zhihu"
@@ -40,10 +70,14 @@ class ZhihuSpider(CrawlSpider):
         info('parsed ' + str(response))
         items = []
         sel = Selector(response)
+
         profile_header = sel.css('.zm-profile-header')[0]
+
         profile_header_main = profile_header.css('.zm-profile-header-main')[0]
         profile_header_operation = profile_header.css('.zm-profile-header-operation')[0]
         profile_header_navbar = profile_header.css('.profile-navbar')[0]
+
+        profile_side_following = sel.css('.zm-profile-side-following')[0]
 
         item = ZhihuPeopleItem()
         item['id'] = urlparse(response.url).path.split('/')[-1]
@@ -69,6 +103,11 @@ class ZhihuSpider(CrawlSpider):
             'collections':'a[href*=collections] .num::text',
             'logs':'a[href*=logs] .num::text',
         }
+        zhihu_profile_side_following = {
+            'followees':'a.item[href*=followees] strong::text',
+            'followers':'a.item[href*=followers] strong::text',
+        }
+
 
         for key, value in zhihu_profile_header_main_dict.items():
             item[key] = [i.extract() for i in profile_header_main.css(value)]
@@ -78,6 +117,9 @@ class ZhihuSpider(CrawlSpider):
 
         for key, value in zhihu_profile_header_navbar.items():
             item[key] = [i.extract() for i in profile_header_navbar.css(value)]
+
+        for key, value in zhihu_profile_side_following.items():
+            item[key] = [i.extract() for i in profile_side_following.css(value)]
 
         items.append(item)
         # import pdb; pdb.set_trace()
