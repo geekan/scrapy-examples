1533d58133a24ce0dea1afc291fce48642feebda
geekan <geekan@foxmail.com>
2014-11-16 16:24:58 +0800

    fix a bug in osx.startproject.sh

--- osx.startproject.sh
+++ osx.startproject.sh
@@ -13,7 +13,7 @@ fi
 echo "Starting project $1."
 
 cp -r template $1
-find ./test -type f | xargs sed -i '' "s/template/$1/"
+find $1 -type f | xargs sed -i '' "s/template/$1/"
 mv $1/template $1/$1
 
 echo "Create $1 succeed!"
