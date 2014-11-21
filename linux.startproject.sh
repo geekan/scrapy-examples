75d06c9fb35f116d999c749d2a338e46dfcd7d85
geekan <geekan@foxmail.com>
2014-11-21 11:58:23 +0800

    add linux.startproject.sh

---
+++ linux.startproject.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+function usage {
+    echo "\n  usage:\n      ./startproject.sh <project name>\n"
+}
+
+if [ "$1" == "" ]; then
+    usage
+    exit
+fi
+
+echo "Starting project $1."
+
+cp -r template $1
+find $1 -type f | xargs sed -i "s/template/$1/"
+mv $1/template $1/$1
+
+echo "Create $1 succeed!"
