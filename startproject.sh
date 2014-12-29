3d44266dad58d697e925c994a323006a41020f39
geekan <geekan@foxmail.com>
2014-12-30 02:02:27 +0800

    use only one startproject.sh

--- startproject.sh
+++ startproject.sh
@@ -5,9 +5,17 @@ function usage {
     echo "\n  usage:\n      ./startproject.sh <project name>\n"
 }
 
-if [ "$1" == "" ]; then
+if [ "$1"X == ""X ]; then
     usage
     exit
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+    alias sed='sed -i'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo `uname -s`
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo `uname -s`
 fi
 
 echo "Starting project $1."
