5193e57804442ffb9145d0962701c29834dec701
geekan <geekan@foxmail.com>
2015-06-04 11:02:15 +0800

    Update startproject.sh
    It seems like a bug? Fixed.

--- startproject.sh
+++ startproject.sh
@@ -10,18 +10,19 @@ if [ "$1"X == ""X ]; then
     exit
 fi
 
-if [ "$(uname)" == "Darwin" ]; then
-    alias sed='sed -i'
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo `uname -s`
-elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    echo `uname -s`
-fi
-
 echo "Starting project $1."
 
 cp -r template $1
-find $1 -type f | xargs sed -i '' "s/template/$1/"
+if [ "$(uname)" == "Darwin" ]; then
+    #alias sed='sed -i'
+    find $1 -type f | xargs sed -i '' "s/template/$1/"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    echo `uname -s`
+    find $1 -type f | xargs sed -i "s/template/$1/"
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    echo `uname -s`
+    find $1 -type f | xargs sed -i "s/template/$1/"
+fi
 mv $1/template $1/$1
 
 echo "Create $1 succeed!"
