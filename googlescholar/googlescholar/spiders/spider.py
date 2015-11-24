46329c8e45302e1b072af265767ea34d2f2ccced
geekan <geekan@foxmail.com>
2015-11-25 01:34:08 +0800

    add content

--- googlescholar/googlescholar/spiders/spider.py
+++ googlescholar/googlescholar/spiders/spider.py
@@ -20,15 +20,6 @@ from misc.log import *
 from misc.spider import CommonSpider
 
 
-import pprint
-class MyPrettyPrinter(pprint.PrettyPrinter):
-    def format(self, object, context, maxlevels, level):
-        if isinstance(object, unicode):
-            return (object.encode('utf8'), True, False)
-        return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
-pp = MyPrettyPrinter()
-
-
 class googlescholarSpider(CommonSpider):
     name = "googlescholar"
     allowed_domains = ["googlescholar.com"]
--- template/template/spiders/spider.py
+++ template/template/spiders/spider.py
@@ -20,15 +20,6 @@ from misc.log import *
 from misc.spider import CommonSpider
 
 
-import pprint
-class MyPrettyPrinter(pprint.PrettyPrinter):
-    def format(self, object, context, maxlevels, level):
-        if isinstance(object, unicode):
-            return (object.encode('utf8'), True, False)
-        return pprint.PrettyPrinter.format(self, object, context, maxlevels, level)
-pp = MyPrettyPrinter()
-
-
 class templateSpider(CommonSpider):
     name = "template"
     allowed_domains = ["template.com"]
