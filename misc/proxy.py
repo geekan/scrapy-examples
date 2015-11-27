e684949ae1b3680d5a87bf5c2959c0c9b61cae8b
geekan <geekan@foxmail.com>
2015-11-27 08:46:02 +0000

    add a proxy from proxy list

--- misc/proxy.py
+++ misc/proxy.py
@@ -4,5 +4,6 @@ If you want to disable it, plz configure settings.py
 '''
 PROXIES = [
     #{"ip_port": "127.0.0.1:8087"}, #goagent
-    {"ip_port": "127.0.0.1:8118"}, #tor via privoxy
+    #{"ip_port": "127.0.0.1:8118"}, #tor via privoxy
+    {"ip_port": "43.245.202.120:8080"}, #tor via privoxy
 ]
