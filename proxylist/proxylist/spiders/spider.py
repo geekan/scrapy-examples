43d9f684e27bcea3f6dab3e536bc40ff0c854a40
geekan <geekan@foxmail.com>
2015-11-28 11:53:13 +0000

    add proxylist

---
+++ proxylist/proxylist/spiders/spider.py
@@ -0,0 +1,37 @@
+import re
+import json
+from urlparse import urlparse
+import urllib
+import pdb
+
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spider import Spider
+except:
+    from scrapy.spider import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+
+
+from proxylist.items import *
+from misc.log import *
+from misc.spider import CommonSpider
+
+
+class proxylistSpider(CommonSpider):
+    name = "proxylist"
+    allowed_domains = ["proxylist.com"]
+    start_urls = [
+        "http://www.proxylist.com/",
+    ]
+    rules = [
+        Rule(sle(allow=("/topsites/category;?[0-9]*/Top/World/Chinese_Simplified_CN/.*$")), callback='parse_1', follow=True),
+    ]
+
+    def parse_1(self, response):
+        info('Parse '+response.url)
+        # x = self.parse_with_rules(response, self.content_css_rules, dict)
+        # pp.pprint(x)
+        # return self.parse_with_rules(response, self.css_rules, proxylistItem)
