1003447ecadd9d25b0e4a0c30edf2cc6a8f8f457
geekan <geekan@foxmail.com>
2016-01-13 23:58:34 +0800

    create douban movie

---
+++ doubanmovie/doubanmovie/spiders/spider.py
@@ -0,0 +1,57 @@
+import re
+import json
+from urlparse import urlparse
+import urllib
+import pdb
+
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spider import Spider
+except:
+    from scrapy.spider import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor as sle
+
+
+from doubanmovie.items import *
+from misc.log import *
+from misc.spider import CommonSpider
+
+
+class doubanmovieSpider(CommonSpider):
+    name = "doubanmovie"
+    allowed_domains = ["doubanmovie.com"]
+    start_urls = [
+        "http://www.doubanmovie.com/",
+    ]
+    rules = [
+        Rule(sle(allow=("/topsites/category;?[0-9]*/Top/World/Chinese_Simplified_CN/.*$")), callback='parse_1', follow=True),
+    ]
+
+    list_css_rules = { 
+        '.linkto': {
+            'url': 'a::attr(href)',
+            'name': 'a::text',
+        }   
+    }   
+
+    list_css_rules_2 = { 
+        '#listZone .Q-tpWrap': {
+            'url': '.linkto::attr(href)',
+            'name': '.linkto::text'
+        }   
+    }   
+
+    content_css_rules = { 
+        'text': '#Cnt-Main-Article-QQ p *::text',
+        'images': '#Cnt-Main-Article-QQ img::attr(src)',
+        'images-desc': '#Cnt-Main-Article-QQ div p+ p::text',
+    }
+
+    def parse_1(self, response):
+        info('Parse '+response.url)
+        # x = self.parse_with_rules(response, self.content_css_rules, dict)
+        # pp.pprint(x)
+        # return self.parse_with_rules(response, self.css_rules, doubanmovieItem)
