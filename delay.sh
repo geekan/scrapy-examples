c1a96ffac69b72ad869df6dc1045726626d2884f
geekan <geekan@foxmail.com>
2016-06-12 16:08:13 +0000

    change delay.sh for linux-gnu

--- delay.sh
+++ delay.sh
@@ -1,2 +1,9 @@
+
+delay=${1:-1}
+
 #find . | grep -E "settings.py$" | xargs sed -i '' "s/DOWNLOAD_DEALY = [0-9]+/DOWNLOAD_DELAY=$1/g"
-find . | grep -E "settings.py$" | xargs sed -E -i '' "s/DOWNLOAD_DELAY = [0-9]+/DOWNLOAD_DELAY = $1/g"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+find . | grep -E "settings.py$" | xargs sed -E -i '' "s/DOWNLOAD_DELAY = [0-9]+/DOWNLOAD_DELAY = $delay/g"
+else
+find . | grep -E "settings.py$" | xargs sed -E -i "s/DOWNLOAD_DELAY = [0-9]+/DOWNLOAD_DELAY = $delay/g"
+fi
