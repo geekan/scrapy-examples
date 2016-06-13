340181aa566df239f7ab223801f16f2eb87e0747
geekan <geekan@foxmail.com>
2016-06-13 15:59:23 +0000

    add github_trending repo.

---
+++ github_trending/github_trending/pipelines.py
@@ -0,0 +1,50 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+import redis
+
+
+from scrapy import signals
+
+
+import json
+import codecs
+from collections import OrderedDict
+
+
+class JsonWithEncodingPipeline(object):
+
+    def __init__(self):
+        self.file = codecs.open('data_utf8.json', 'w', encoding='utf-8')
+
+    def process_item(self, item, spider):
+        line = json.dumps(OrderedDict(item), ensure_ascii=False, sort_keys=False) + "\n"
+        self.file.write(line)
+        return item
+
+    def close_spider(self, spider):
+        self.file.close()
+
+
+class RedisPipeline(object):
+
+    def __init__(self):
+        self.r = redis.StrictRedis(host='localhost', port=6379)
+
+    def process_item(self, item, spider):
+        if not item['id']:
+            print 'no id item!!'
+
+        str_recorded_item = self.r.get(item['id'])
+        final_item = None
+        if str_recorded_item is None:
+            final_item = item
+        else:
+            ritem = eval(self.r.get(item['id']))
+            final_item = dict(item.items() + ritem.items())
+        self.r.set(item['id'], final_item)
+
+    def close_spider(self, spider):
+        return
---
+++ github_trending/github_trending/spiders/spider.py
@@ -0,0 +1,52 @@
+import re
+import json
+from urlparse import urlparse
+import urllib
+import pdb
+
+
+from scrapy.selector import Selector
+try:
+    from scrapy.spiders import Spider
+except:
+    from scrapy.spiders import BaseSpider as Spider
+from scrapy.utils.response import get_base_url
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor as sle
+
+
+from github_trending.items import *
+from misc.log import *
+from misc.spider import CommonSpider
+
+
+class github_trendingSpider(CommonSpider):
+    name = "github_trending"
+    allowed_domains = ["github.com"]
+    start_urls = [
+        "http://www.github.com/trending",
+    ]
+    rules = [
+        Rule(sle(allow=("/trending$")), callback='parse_1', follow=True),
+    ]
+
+    list_css_rules = { 
+        '.repo-list-item': {
+            'repo_name': '.repo-list-name a::attr(href)',
+            'repo_meta': '.repo-list-meta::text',
+        }   
+    }   
+
+    content_css_rules = { 
+        'text': '#Cnt-Main-Article-QQ p *::text',
+        'images': '#Cnt-Main-Article-QQ img::attr(src)',
+        'images-desc': '#Cnt-Main-Article-QQ div p+ p::text',
+    }
+
+    def parse_1(self, response):
+        info('Parse '+response.url)
+        x = self.parse_with_rules(response, self.list_css_rules, dict)
+        # x = self.parse_with_rules(response, self.content_css_rules, dict)
+        print(json.dumps(x, ensure_ascii=False, indent=2))
+        # pp.pprint(x)
+        # return self.parse_with_rules(response, self.css_rules, github_trendingItem)
